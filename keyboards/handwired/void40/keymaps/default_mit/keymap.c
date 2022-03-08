05cb5df5f8e506d0031499c25c149893989f9764
andek <adam.kule@gmail.com>
2022-03-08 23:09:47 +0100

    Changes LALT to RALT for polish chars

--- keyboards/handwired/void40/keymaps/default_mit/keymap.c
+++ keyboards/handwired/void40/keymaps/default_mit/keymap.c
@@ -41,13 +41,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,       KC_T,     KC_Y,     KC_U,       KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,       KC_G,     KC_H,     KC_J,       KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,       KC_B,     KC_N,     KC_M,       KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-    KC_LCTL, KC_LGUI,   KC_LGUI, KC_LALT, MO(_LOWER),      KC_SPC,        MO(_RAISE), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    KC_LCTL, KC_LGUI,   KC_LGUI, KC_RALT, MO(_LOWER),      KC_SPC,        MO(_RAISE), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 [_APEX] = LAYOUT_planck_mit(
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,       KC_T,     KC_Y,     KC_U,       KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,       KC_G,     KC_H,     KC_J,       KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,       KC_B,     KC_N,     KC_M,       KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-    KC_LCTL, KC_1, KC_3,    KC_LALT, KC_4,       KC_SPC,    MO(_RAISE), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    KC_LCTL, KC_1, KC_3,    KC_RALT, KC_4,       KC_SPC,    MO(_RAISE), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 /* Lower
@@ -107,58 +107,4 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 layer_state_t layer_state_set_user(layer_state_t state) {
     return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
-}
-
-/*
-/ UNCOMMENT FOR ROTARY ENCODER
-/
-
-static bool tabbing = false;
-static uint16_t tabtimer;
-#define TABBING_TIMER 750
-
-void encoder_update_user(uint8_t index, bool clockwise) {
-  if (index == 0) {
-    if (IS_LAYER_ON(_RAISE)) {
-      if (clockwise) {
-        tabtimer = timer_read();
-        if(!tabbing) {
-          register_code(KC_LALT);
-          tabbing = true;
-        }
-        tap_code(KC_TAB);
-      } else {
-        tabtimer = timer_read();
-        if(!tabbing) {
-          register_code(KC_LALT);
-          tabbing = true;
-        }
-
-        register_code(KC_LSFT);
-        tap_code(KC_TAB);
-        unregister_code(KC_LSFT);
-      }
-    } else {
-      if (clockwise) {
-        tap_code(KC_VOLU);
-      } else {
-        tap_code(KC_VOLD);
-      }
-    }
-  }
-}
-
-void matrix_scan_user(void) {
-  if(tabbing) {
-    if (timer_elapsed(tabtimer) > TABBING_TIMER) {
-      unregister_code(KC_LALT);
-      tabbing = false;
-    }
-  }
-}
-
-*/
-
-void matrix_init_user(void) {
-
 }
