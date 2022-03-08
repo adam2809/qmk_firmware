805ca854d571f9f7b96b219d77486e0dba43f443
andek <adam.kule@gmail.com>
2022-03-08 23:28:33 +0100

    Makes apex layer implementetion clearer

--- keyboards/handwired/void40/keymaps/default_mit/keymap.c
+++ keyboards/handwired/void40/keymaps/default_mit/keymap.c
@@ -41,13 +41,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,       KC_T,     KC_Y,     KC_U,       KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,       KC_G,     KC_H,     KC_J,       KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,       KC_B,     KC_N,     KC_M,       KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-    KC_LCTL, KC_LGUI,   KC_LGUI, KC_RALT, MO(_LOWER),      KC_SPC,        MO(_RAISE), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    KC_LCTL, KC_LGUI, KC_NO, KC_RALT, MO(_LOWER),      KC_SPC,        MO(_RAISE), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 [_APEX] = LAYOUT_planck_mit(
-    KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,       KC_T,     KC_Y,     KC_U,       KC_I,    KC_O,    KC_P,    KC_BSPC,
-    KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,       KC_G,     KC_H,     KC_J,       KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,       KC_B,     KC_N,     KC_M,       KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-    KC_LCTL, KC_1, KC_3,    KC_RALT, KC_4,       KC_SPC,    MO(_RAISE), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
+    _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
+    _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
+    _______,KC_1   ,KC_3   ,_______,KC_4   ,    _______    ,_______,_______,_______,_______,_______
 ),
 
 /* Lower
@@ -108,3 +108,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 layer_state_t layer_state_set_user(layer_state_t state) {
     return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
 }
+
+
+// [_NADA] = LAYOUT_planck_mit(
+//     _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
+//     _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
+//     _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
+//     _______,_______,_______,_______,_______,   _______     ,_______,_______,_______,_______,_______
+// ),
